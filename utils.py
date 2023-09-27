b19205122d8e3eb2b3c8a78290e5ca0f56c9a10e
s3nh <17111860+s3nh@users.noreply.github.com>
2023-09-27 14:42:47 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,3 +8,18 @@ def similarity_calc(embeda: torch.Tensor, embedb: torch.Tensor, normalize: bool 
         embeda = F.normalize(embeda)
         embedb = F.normalize(embedb)
     return embeda @ embedb.T
+
+
+###
+x  = []
+for chunk in tqdm(all_sentence_scawk_chunk):
+    #print(chunk)
+    chunk_tokenized = tr.tokenizer(chunk, return_tensors=TranslatorCFG.return_tensors, padding=True,  truncation=True)
+    #print(chunk_tokenized)
+    #translated  = tr.retriever.generate(**chunk_tokenized.to(torch.device('cuda')))
+    #x.append([tr.tokenizer.decode(el, skip_special_tokens=True) for el in translated])
+    output = tr.retriever.generate(**chunk_tokenized.to(torch.device('cuda')))
+    trans_chunk = [tr.tokenizer.decode(el, skip_special_tokens=True) for el in output]
+    #x= torch.cat((x, output))
+    x.append(trans_chunk)
+###
