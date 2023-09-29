a2cb5f8d15e75b99085e098758e98994013e64d9
s3nh <17111860+s3nh@users.noreply.github.com>
2023-09-29 22:11:35 +0200

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,10 +1,22 @@
 import numpy as np
 
+class TranslatorCFG:
+    model_name: str = '../assets/ct2-opus-mt-pl-en'
+    tokenizer_name: str = '../assets/opus-mt-pl-en'
+    local_files_only: bool = True
+    chunk_size: int = 12
+    max_length: int = 512
+    return_tensors: str = 'pt'
+    hf_model_file: str = 'pytorch_model.bin'
+    ct2_model_file: str = 'model.bin'
+
 class RetrieverCFG:
-    model_name: str = ''
+    model_name: str = '../assets/bge-large-v1'
     local_files_only: bool = True
     max_length: int = 512
     padding: bool = True
     truncation: bool = True
     return_tensors: str = 'pt'
     chunk_size: int = 16
+    pad_token: str = "PAD "
+    model_half: bool = False 
